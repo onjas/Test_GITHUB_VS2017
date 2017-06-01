bd7e847ce5b6a7ac8c93f03332275c6b7cff4eaa
kevinberret <berret.kevin@gmail.com>
2017-06-01 19:15:08 +0200

    init

---
+++ client/src/main/java/ch/hevs/wiggerberret/client/ClientApplication.java
@@ -0,0 +1,12 @@
+package ch.hevs.wiggerberret.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientApplication.class, args);
+	}
+}
